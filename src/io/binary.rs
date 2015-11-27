6d7846c762eb870ace2e7990f455a6bd3ae2a241
Ashish Myles <marcianx@gmail.com>
2015-11-27 14:08:23 -0500

    io: Binary trait impl for mesh field status.

--- src/io/binary.rs
+++ src/io/binary.rs
@@ -1,12 +1,15 @@
 
 extern crate byteorder;
+extern crate num;
 
 use self::byteorder::{BigEndian, LittleEndian, ReadBytesExt, WriteBytesExt};
+use self::num::Zero;
 use std::io::{Read, Write};
 use std::mem;
 
 use geometry::vector::*;
 use io::result::{Error, Result};
+use mesh::status::{FlagBits, Status};
 
 const UNKNOWN_SIZE: usize = !0usize;
 
@@ -382,7 +385,48 @@ mod test_geometry_vec {
 ////////////////////////////////////////////////////////////////////////////////
 // Implementations for mesh status.
 
-// TODO
+impl Binary for Status {
+    fn is_streamable() -> bool { true }
+    fn size_of_type() -> usize { mem::size_of::<Self>() }
+
+    fn store(&self, writer: &mut Write, swap: bool) -> Result<usize> {
+        self.bits().store(writer, swap)
+    }
+
+    fn restore(&mut self, reader: &mut Read, swap: bool) -> Result<usize> {
+        let mut bits: FlagBits = Zero::zero();
+        let len = try!(bits.restore(reader, swap));
+        *self = Status::from_bits_truncate(bits);
+        Ok(len)
+    }
+}
+
+
+#[cfg(test)]
+mod test_status {
+    use io::binary::test;
+    use mesh::status;
+    
+    #[test]
+    fn test_store() {
+        let flags1 = status::DELETED | status::LOCKED | status::HIDDEN | status::TAGGED | status::TAGGED2;
+        test::test_store(false, &flags1, &[0x6b, 0x00, 0x00, 0x00]);
+        test::test_store(true , &flags1, &[0x00, 0x00, 0x00, 0x6b]);
+        let flags2 = status::SELECTED | status::FEATURE | status::FIXED_NONMANIFOLD;
+        test::test_store(false, &flags2, &[0x94, 0x00, 0x00, 0x00]);
+        test::test_store(true , &flags2, &[0x00, 0x00, 0x00, 0x94]);
+    }
+
+    #[test]
+    fn test_restore() {
+        let flags1 = status::DELETED | status::LOCKED | status::HIDDEN | status::TAGGED | status::TAGGED2;
+        test::test_restore(false, &[0x6b, 0xff, 0xff, 0xff], status::Status::empty, &flags1);
+        test::test_restore(true , &[0xff, 0xff, 0xff, 0x6b], status::Status::empty, &flags1);
+        let flags2 = status::SELECTED | status::FEATURE | status::FIXED_NONMANIFOLD;
+        test::test_restore(false, &[0x94, 0xff, 0xff, 0xff], status::Status::empty, &flags2);
+        test::test_restore(true , &[0xff, 0xff, 0xff, 0x94], status::Status::empty, &flags2);
+    }
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 // Implementation for exact-size iterators of primitives.
