ad23e42ea51300735e3b74063119c631160bba47
Ashish Myles <marcianx@gmail.com>
2015-12-26 23:38:34 -0500

    util: Make property::Handle not parameterized by index type.

--- src/util/property/handle.rs
+++ src/util/property/handle.rs
@@ -1,10 +1,8 @@
 extern crate num;
 use std::fmt::{Display, Formatter};
-use std::ops::Not;
-use self::num::traits::Zero;
 
 /// This is the default index type.
-pub type IndexType = u32;
+pub type Index = u32;
 
 /// Trait for forward and backward traversal.
 pub trait Ordinal {
@@ -14,26 +12,31 @@ pub trait Ordinal {
     fn pred(&self) -> Self;
 }
 
-impl Ordinal for u32 {
-    fn succ(&self) -> Self { self + 1u32 }
-    fn pred(&self) -> Self { self - 1u32 }
+impl Ordinal for Index {
+    fn succ(&self) -> Self { self + (1 as Index) }
+    fn pred(&self) -> Self { self - (1 as Index) }
 }
 
 #[derive(Eq, PartialEq, Clone, Hash, Debug, Copy)]
 /// BaseHandle for all index types.
-pub struct Handle<Index> { idx: Index }
+pub struct Handle { idx: Index }
 
-impl<Index: Copy + Ordinal + Eq + Not<Output=Index> + Zero> Handle<Index> {
+impl Handle {
     /// Returns the value corresponding to an invalid index.
-    pub fn invalid_index() -> Index { !Index::zero() }
+    pub fn invalid_index() -> Index { Index::max_value() }
 
     /// Initialize a handle with an invalid index.
     pub fn new() -> Self { Handle { idx: Self::invalid_index() } }
     /// Construct from index.
-    pub fn from_index(idx: Index) -> Self { Handle { idx: idx } }
+    pub fn from_index(idx: usize) -> Self {
+        assert!(::std::mem::size_of::<Index>() <= ::std::mem::size_of::<usize>());
+        // Note that `Index::max_value()` is `invalid_index()`.
+        assert!(idx < Index::max_value() as usize);
+        Handle { idx: idx as Index }
+    }
 
     /// Gets the index.
-    pub fn idx(&self) -> Index { self.idx }
+    pub fn index(&self) -> Index { self.idx }
     /// Whether the handle is valid.
     pub fn is_valid(&self) -> bool { self.idx == Self::invalid_index() }
     /// Invalidates the underlying index.
@@ -50,29 +53,29 @@ impl<Index: Copy + Ordinal + Eq + Not<Output=Index> + Zero> Handle<Index> {
 }
 
 // Display trait implementation.
-impl<Index: Display> Display for Handle<Index> {
+impl Display for Handle {
     fn fmt(&self, formatter: &mut Formatter) -> ::std::fmt::Result { self.idx.fmt(formatter) }
 }
 
 /// Handle provider trait for supporting distinct types to wrap Handle.
-pub trait HandleProvider<Index> {
+pub trait HandleProvider {
     /// Returns the underlying handle.
-    fn get_handle(&self) -> &Handle<Index>;
+    fn handle(&self) -> &Handle;
 }
 
-impl<Index> HandleProvider<Index> for Handle<Index> {
-    fn get_handle(&self) -> &Handle<Index> { &self }
+impl HandleProvider for Handle {
+    fn handle(&self) -> &Handle { &self }
 }
 
 #[macro_export]
 macro_rules! def_index {
     ($handle: ident) => {
         #[derive(Eq, PartialEq, Clone, Hash, Debug, Copy)]
-        pub struct $handle<Index>($crate::util::property::Handle<Index>);
-        impl<Index> $crate::util::property::HandleProvider<Index> for $handle<Index> {
-            fn get_handle(&self) -> &$crate::util::property::Handle<Index> { &self.0 }
+        pub struct $handle($crate::util::property::Handle);
+        impl $crate::util::property::HandleProvider for $handle {
+            fn handle(&self) -> &$crate::util::property::Handle { &self.0 }
         }
-        impl<Index: ::std::fmt::Display> ::std::fmt::Display for $handle<Index> {
+        impl ::std::fmt::Display for $handle {
             fn fmt(&self, formatter: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
                 self.0.fmt(formatter)
             }
