35bbb67c621ee91d60723c4d29515f9c18ed42c2
Ashish Myles <marcianx@gmail.com>
2016-04-08 01:12:49 -0400

    Removed #![feature(read_exact)] since it is stabilized.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,5 @@
 #![crate_name = "openmesh"]
 
-// Remove when Read#read_exact() is stabilized (https://github.com/rust-lang/rust/issues/27585).
-#![feature(read_exact)]
-
 #[macro_use(bitflags)]
 extern crate bitflags;
 #[macro_use(impl_downcast)]
