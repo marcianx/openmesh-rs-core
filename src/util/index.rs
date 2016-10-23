11635dd75c3886ea69e86e8783a77e2f39d6dac0
Ashish Myles <marcianx@gmail.com>
2016-10-23 00:20:43 -0400

    Moved BitVec into bitvec-rs (Cargo) and used it here.

--- src/util/index.rs
+++ src/util/index.rs
@@ -39,13 +39,44 @@ impl<T> IndexSet<usize> for Vec<T> {
     }
 }
 
+////////////////////////////////////////////////////////////////////////////////
+// Impls for `BitVec`
+
+use util::bitvec::BitVec;
+
+static TRUE: bool = true;
+static FALSE: bool = false;
+
+impl IndexUnchecked<usize> for BitVec {
+    unsafe fn index_unchecked(&self, index: usize) -> &Self::Output {
+        assert!(index < self.len());
+        let value = self.get_unchecked(index);
+        if value { &TRUE } else { &FALSE }
+    }
+}
+
+impl IndexSetUnchecked<usize> for BitVec {
+    unsafe fn index_set_unchecked(&mut self, index: usize, value: bool) {
+        assert!(index < self.len());
+        self.set_unchecked(index, value);
+    }
+}
+
+impl IndexSet<usize> for BitVec {
+    fn index_set(&mut self, index: usize, value: bool) {
+        self.set(index, value);
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
 
 #[cfg(test)]
 mod test {
     use super::{IndexUnchecked, IndexSetUnchecked, IndexSet};
+    use util::bitvec::BitVec;
 
     #[test]
-    fn test_index() {
+    fn test_index_vec() {
         let mut vec = vec![1, 2, 3];
         unsafe {
             assert_eq!(*vec.index_unchecked(0), 1);
@@ -66,5 +97,32 @@ mod test {
         vec.index_set(2, 3);
         assert_eq!(vec[2], 3);
         assert_eq!(vec, vec![1, 2, 3]);
+    }
+
+    #[test]
+    fn test_index_bitvec() {
+        let mut vec = BitVec::from_bytes(&[0xef, 0xa5, 0x71]);
+        assert_eq!(vec[0], true);
+        assert_eq!(vec[4], false);
+        assert_eq!(vec[15], true);
+        unsafe {
+            assert_eq!(*vec.index_unchecked(0), true);
+            assert_eq!(*vec.index_unchecked(4), false);
+            assert_eq!(*vec.index_unchecked(15), true);
+        }
+
+        unsafe {
+            vec.index_set_unchecked(0, false);
+            assert_eq!(*vec.index_unchecked(0), false);
+            vec.index_set_unchecked(15, false);
+            assert_eq!(*vec.index_unchecked(15), false);
+        }
+        assert_eq!(vec.as_bytes(), &[0xee, 0x25, 0x71]);
+
+        vec.index_set(0, true);
+        assert_eq!(vec[0], true);
+        vec.index_set(15, true);
+        assert_eq!(vec[15], true);
+        assert_eq!(vec.as_bytes(), &[0xef, 0xa5, 0x71]);
     }
 }
--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,2 +1,6 @@
+extern crate bitvec_rs;
+
 pub mod index;
-pub mod bitvec;
+pub mod bitvec {
+    pub use super::bitvec_rs::BitVec;
+}
